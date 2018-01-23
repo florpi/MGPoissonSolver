05f502e72bd85c26f9be985cd051aeaff2659007
florpi <florpisoteadadejardin@gmail.com>
2018-01-23 11:27:45 +0000

    parallel v2

--- results/plot.py
+++ results/plot.py
@@ -2,12 +2,12 @@ import numpy as np
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 
-r, acc = np.loadtxt("acceleration2560.txt",unpack=True, skiprows=0)
+r, acc = np.loadtxt("acceleration256.txt",unpack=True, skiprows=0)
 
 y = np.linspace(0.00001, 1000.,100)
-x =(1./2560)*np.ones(100)
+x =(1./256)*np.ones(100)
 
-r_theo = np.linspace(0.001,0.6,100)
+r_theo = np.linspace(0.002,0.6,100)
 
 
 gs = gridspec.GridSpec(2, 2,
@@ -28,7 +28,7 @@ ax2.scatter(r, residual, s=1, color='black')
 ax2.set_xlabel("distance")
 ax2.set_ylabel("Residual")
 ax2.plot(x,y,"--",linewidth=2,color='coral')
-plt.savefig("results.png")
+plt.savefig("result.png")
 plt.show()
 
 
